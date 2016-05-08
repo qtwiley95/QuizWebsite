0b45be1d79eb0dcc854aa191e5224d23d403a93c
Matthew Bauer <mjbauer95@gmail.com>
2016-05-08 13:23:07 -0500

    check if user exists before create.

--- create-user.php
+++ create-user.php
@@ -16,12 +16,23 @@ if (isset($_POST["name"]) && isset($_POST["pass"])) {
     'cost' => 11,
   ]);
 
-  $insert = $conn->prepare("INSERT INTO 368_users (user, pass) VALUES (?, ?)");
-  $insert->bind_param("ss", $person, $hashed);
-  $result = $insert->execute();
-  $insert->close();
+  $select = $conn->prepare("SELECT * FROM 368_users WHERE user = ?");
+  $select->bind_param("s", $person);
+  $select->execute();
+  $select->store_result();
 
-  echo "okay";
+  if($select->num_rows == 0) {
+    $insert = $conn->prepare("INSERT INTO 368_users (user, pass) VALUES (?, ?)");
+    $insert->bind_param("ss", $person, $hashed);
+    $result = $insert->execute();
+    $insert->close();
+
+    echo "okay";
+  } else {
+    echo "user already exists";
+  }
+
+  $select->close();
 } else {
   echo "fail";
 }

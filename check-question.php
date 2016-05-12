7d6a860ddac2958e29dbca3461fe57046c2e7a6a
Matthew Bauer <mjbauer95@gmail.com>
2016-05-12 10:54:06 -0500

    Keep track of correct/incorrect answers.

--- check-question.php
+++ check-question.php
@@ -1,31 +1,45 @@
 <?php
-if (isset($_POST["id"]) && !empty($_POST["id"]) && isset($_POST['answer']) && !empty($_POST["answer"])) {
 
-$conn = new mysqli("mysql.eecs.ku.edu", "qwiley", "asdf", "qwiley");
+session_start();
 
-if($conn == false) {
-  echo "connection failed";
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+if (isset($_POST["id"]) && !empty($_POST["id"]) && isset($_POST['answer']) && !empty($_POST["answer"]) && isset($_SESSION["login"])) {
+  $conn = new mysqli("mysql.eecs.ku.edu", "qwiley", "asdf", "qwiley");
+
+  if($conn == false) {
+    echo "connection failed";
+    $conn->close();
+    exit();
+  }
+
+  $select = $conn->prepare("SELECT answer FROM 368_questions WHERE id = ?");
+  $select->bind_param("s", $_POST["id"]);
+  $select->execute();
+  $select->bind_result($answer);
+  $select->fetch();
+  $select->close();
+
+  $isCorrect = ($answer == $_POST["answer"]);
+
+  if ($isCorrect) {
+    $update = $conn->prepare("UPDATE 368_users SET correct_answers = correct_answers + 1 WHERE user = ?");
+    $update->bind_param("s", $_SESSION["login"]);
+    $update->execute();
+    $update->close();
+  } else {
+    $update = $conn->prepare("UPDATE 368_users SET incorrect_answers = incorrect_answers + 1 WHERE user = ?");
+    $update->bind_param("s", $_SESSION["login"]);
+    $update->execute();
+    $update->close();
+  }
+
+  $out = array("success" => $isCorrect);
+  echo json_encode($out);
+
   $conn->close();
-  exit();
-}
-
-$select = $conn->prepare("SELECT answer FROM 368_questions WHERE id = ?");
-$select->bind_param("s", $_POST["id"]);
-$select->execute();
-$select->bind_result($answer);
-$select->fetch();
-
-if ($answer == $_POST["answer"]) {
-  $out = array("success" => True);
-  echo json_encode($out);
-} else {
-  $out = array("success" => False);
-  echo json_encode($out);
-}
-
-$select->close();
-$conn->close();
 }else{
-    echo "ERROR: Bad request.";
+  echo "ERROR: Bad request.";
 }
 ?>

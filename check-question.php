4ff89bcb96af3c9b03a8b8884a6fb6d82a92083b
Matthew Bauer <mjbauer95@gmail.com>
2016-05-12 12:57:03 -0500

    fix typo

--- check-question.php
+++ check-question.php
@@ -12,7 +12,7 @@ if (isset($_POST["id"]) && !empty($_POST["id"]) && isset($_POST['answer']) && !e
     $conn->close();
     exit();
   }
-  
+
   function handleValidation($db,$questionId,$givenAnswer) {
   	 $isCorrect = validateAnswer($db,$questionId,$givenAnswer);
  	 $isCorrect ? addCorrect($db,$_SESSION["login"]) : addIncorrect($db,$_SESSION["login"]);
@@ -20,27 +20,27 @@ if (isset($_POST["id"]) && !empty($_POST["id"]) && isset($_POST['answer']) && !e
  	 $out = array("success" => $isCorrect, "correct" => $performance["correct_answers"], "incorrect" => $performance["incorrect_answers"]);
  	 return $out;
   }
-  
+
   function validateAnswer($db,$id,$given) {
   	 $sql = "SELECT answer FROM 368_questions WHERE id = $id";
   	 $answer =  $db->query($sql)->fetch_assoc();
   	 return $answer["answer"] == $given;
   }
-  
+
   function addCorrect($db,$user){
   	 $db->query("UPDATE 368_users SET correct_answers = correct_answers + 1 WHERE user = '$user'");
   }
-  
+
   function addIncorrect($db,$user){
-  	 $db->query("UPDATE 368_users SET incorrect_answers = correct_answers + 1 WHERE user = '$user'");
+  	 $db->query("UPDATE 368_users SET incorrect_answers = incorrect_answers + 1 WHERE user = '$user'");
   }
-  
+
   function getPerformance($db,$user) {
 	 $sql = "SELECT incorrect_answers, correct_answers FROM 368_users WHERE user = '$user'";
   	 $performance = $db->query($sql)->fetch_assoc();
   	 return $performance;
   }
-  
+
   header('Content-Type: application/json');
   echo json_encode(handleValidation($conn,$_POST["id"],$_POST["answer"]));
 

9f8382cd1254cf026f602675121bab7850458736
Matthew Bauer <mjbauer95@gmail.com>
2016-05-12 12:49:49 -0500

    Fix session starts.

--- add_question.php
+++ add_question.php
@@ -1,5 +1,4 @@
 <?php
-  session_start();
 
   session_start ();
   if (!isset ($_SESSION['login'])) {
--- get-questions.php
+++ get-questions.php
@@ -2,6 +2,8 @@
 
 include "qwiley_get_questions.php";
 
+session_start();
+
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 

3e22cd86448fcbbeaa6552e1679df3bb9cd457a9
Francinaldo Almeida <fap_sb@globomail.com>
2016-05-13 11:32:49 -0500

    Comment get-question

--- get-questions.php
+++ get-questions.php
@@ -5,20 +5,37 @@ session_start();
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
+// Check if is a valid request
 if (isset($_GET["subject"]) && !empty($_GET["subject"])) {
+  /**
+   * The connection with database
+   *
+   * @var mysqli_connection
+   */
   $conn = new mysqli("mysql.eecs.ku.edu", "qwiley", "asdf", "qwiley");
 
+  // Check connection with database
   if($conn == false) {
     echo "connection failed";
     $conn->close();
     exit();
   }
 
+  // Set the maximum number of questions to be returned
   $max_results = 1;
   if (isset($_GET["max_results"])) {
     $max_results = intval($_GET["max_results"]);
   }
 
+  /**
+   * Works with the database to fetch the questions
+   * according with the subject given.
+   *
+   * @param msqli_connection $db
+   * @param string           $subject
+   * @param int              @maxResults
+   * @return Array
+   */
   function getQuestions($db,$subject,$maxResults)
   {
     $sql = "SELECT * FROM 368_questions WHERE subject = '$subject' ORDER BY RAND() LIMIT $maxResults";
@@ -42,6 +59,7 @@ if (isset($_GET["subject"]) && !empty($_GET["subject"])) {
     }
   }
 
+  // Set the response header and show the response in json format
   header('Content-Type: application/json');
   echo json_encode(getQuestions($conn,$_GET["subject"],$max_results));
 
